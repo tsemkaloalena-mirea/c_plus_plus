bfadde7a715ce414e327620dbc01763f9d96c3bb
Чокопайка <73431027+tsemkaloalena-mirea@users.noreply.github.com>
2020-12-07 16:00:34 +0300

    Update Шарики.cpp

--- Шарики.cpp
+++ Шарики.cpp
@@ -2,31 +2,17 @@
 using namespace std;
 
 int n = 10;
-int* list = new int[n];
 
 int perestanovka(int m, int n) {
-	if (m < n-1) {
-		int j, f = 1;
-		for (j = 1; j < n - m; j++) {
-			f *= j;
-		}
-		
-		return f + perestanovka(m + 1, n);
+	if (m > 2) {
+		return (perestanovka(m - 1, n) + perestanovka(m - 2, n)) * (m - 1);
 	}
 	else {
-		return 0;
+		return 1;
 	}
 }
 
 
 int main() {
-	int i;
-	for (i = 1; i <= n; i++) {
-		list[i - 1] = i;
-	}
-	i = 1;
-	int s = 0;
-	s += perestanovka(0, n);
-	cout << s << endl;
-
+	cout << perestanovka(n, n) << endl;
 }
